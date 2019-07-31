4a3b62ed4d62a569d0522c8b75e238ad07dc09f3
lingkunming <lingkunming@foxmail.com>
2019-07-31 13:09:01 +0800

    上传尚硅谷佟刚老师的代码

---
+++ spring-boot-01-helloworld/src/main/java/com/HelloController.java
@@ -0,0 +1,18 @@
+package com;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class HelloController {
+
+    @ResponseBody
+    @RequestMapping("/hello")
+
+    public String hello(){
+        System.out.println('a');
+        return "Hello World!";
+    }
+}
